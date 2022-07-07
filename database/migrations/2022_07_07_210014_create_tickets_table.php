795cf123740c4ecd1351fa1e617ab59cd36c3789
Cristian Guaman <cristian.guaman@icloud.com>
2022-07-07 16:25:31 -0500

    feat: add tickets migration

---
+++ database/migrations/2022_07_07_210014_create_tickets_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string("origin_city");
+            $table->string("destination_city");
+            $table->string("return_date");
+            $table->string("price");
+            $table->string("airline");
+            $table->string("number_scales");
+            $table->string("flight_duration");
+            $table->foreignId("user_id");
+
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
