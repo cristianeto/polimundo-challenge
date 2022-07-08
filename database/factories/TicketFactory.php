bcd5d0a3636645182224188f410e20b0f77b2953
Cristian Guaman <cristian.guaman@icloud.com>
2022-07-08 12:46:51 -0500

    refactor: update flight_duration field in TicketFactory

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -20,11 +20,11 @@ class TicketFactory extends Factory
         return [
             'origin_city' => fake()->city(),
             "destination_city" => fake()->city(),
-            "return_date" =>fake()->dateTime(),
-            "price" =>fake()->randomFloat($nbMaxDecimals = 2, $min = 500, $max = 1000),
-            "airline" =>fake()->company(),
-            "number_scales" =>fake()->numberBetween($int1 = 1, $int2 = 5),
-            "flight_duration" =>fake()->text(),
+            "return_date" => fake()->dateTime(),
+            "price" => fake()->randomFloat($nbMaxDecimals = 2, $min = 500, $max = 1000),
+            "airline" => fake()->company(),
+            "number_scales" => fake()->numberBetween($int1 = 1, $int2 = 5),
+            "flight_duration" => fake()->numberBetween($int1 = 1, $int2 = 25)." hours",
             "user_id" => fake()->numberBetween($int1 = 1, $int2 = 10),
         ];
     }
